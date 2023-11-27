bf69e7e3db89cf268e4369dbc1af50816e4f0c67
Atul Singh <cocatul11@gmail.com>
2023-11-28 02:53:52 +0530

    committed stack code

--- Stack/StackImplementation.c
+++ Stack/StackImplementation.c
@@ -1,106 +1,205 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define SIZE 50
 
-typedef struct stack {
-	int ar[SIZE];
-	int top;
-}st;
+typedef struct Linked {
+  int val;
+  struct Linked * next;
+}
+lnk;
 
-void push (st *, int);
-int pop (st *);
-int peek (st);
-int isEmpty (st);
-int isFull (st);
-void display (st);
+lnk * insertbeg(lnk * , int);
+lnk * insertend(lnk * , int);
+lnk * insertatpos(lnk * , int, int);
+lnk * del(lnk * , int);
+lnk * insertbefore(lnk * , int);
+void disp(lnk * );
+void insertafter(lnk * , int);
 
-void main () {
-	st s;
-	s.top = -1;
-	
-	int choice, value, poppedValue;
-	
-	while(1) {
-		printf("\n1) Push \n2) Pop \n3) Peek \n4) Display \n5) Exit");
-		printf("\nEnter your Choice >> ");
-		scanf("%d", &choice);
-		
-		switch(choice) {
-			case 1:
-				if( !isFull(s) ) {
-					printf("\nInput the value >> ");
-					scanf("%d", &value);
-					push(&s, value);
-					break;
-				} else {
-					printf("\nNo more Space \n");
-					break;
-				}
-			case 2:
-				if( !isEmpty(s) ) {
-					poppedValue = pop(&s);
-					printf("\nPopped value is >> %d \n", poppedValue);
-					break;
-				} else {
-					printf("\nThere is nothing to Display \n");
-					break;
-				}
-			case 3:
-				if( !isEmpty(s) ) {
-					printf("\nTopmost value is >> %d \n", peek(s));
-					break;
-				} else {
-					printf("\nThere is nothing to Display \n");
-					break;
-				}
-			case 4:
-				if( !isEmpty(s) ) {
-					printf("\nValues in the Stack are : \n");
-					display(s);
-					break;
-				} else {
-					printf("\nThere is nothing to Display \n");
-					break;
-				}
-			case 5:
-				exit(0);
-			default:
-				printf("\nWrong Choice \n");
-		}
-	}
+int main() {
+  lnk * head = NULL;
+  int ch, v, p;
+  while (1) {
+    printf("\n1) Insert End\n2) Insert begining\n3) Insert at Position\n4) Insert after a certain node\n5) Insert before a certain node\n6) Delete\n7) Display\n8) Exit");
+    printf("\n Enter your choice : ");
+    scanf("%d", & ch);
+    switch (ch) {
+    case 1:
+      printf("\nEnter the value to be inserted : ");
+      scanf("%d", & v);
+      head = insertend(head, v);
+      break;
+    case 2:
+      printf("\nEnter the value to be inserted : ");
+      scanf("%d", & v);
+      head = insertbeg(head, v);
+      break;
+    case 3:
+      printf("\nEnter the value to be inserted : ");
+      scanf("%d", & v);
+      printf("\n Enter the position where to be inserted : ");
+      scanf("%d", & p);
+      head = insertatpos(head, v, p);
+      break;
+
+    case 4:
+      printf("\nEnter the value after which you want to Insert : ");
+      scanf("%d", & v);
+      insertafter(head, v);
+      break;
+    case 5:
+      printf("\nEnter the value before which you want to Insert : ");
+      scanf("%d", & v);
+      head = insertbefore(head, v);
+      break;
+    case 6:
+      printf("\n Enter the element to be deleted : ");
+      scanf("%d", & v);
+      head = del(head, v);
+      break;
+    case 7:
+      printf("\n Values in the linked list are : ");
+      disp(head);
+      break;
+    case 8:
+      exit(0);
+    default:
+      printf("\n Wrong choice ");
+    }
+  }
 }
 
-void push (st *s, int value) {
-	s->ar[++s->top] = value;
+lnk * insertbeg(lnk * h, int g) {
+  lnk * temp;
+  temp = (lnk * ) malloc(sizeof(lnk));
+  temp -> val = g;
+  temp -> next = h;
+  h = temp;
+  return h;
 }
 
-int pop (st *s) {
-	return s->ar[s->top--];
+lnk * insertend(lnk * h, int g) {
+  lnk * temp, * ptr;
+  temp = (lnk * ) malloc(sizeof(lnk));
+  temp -> val = g;
+  temp -> next = NULL;
+  if (h == NULL) {
+    printf("\n This is going to be first/head node");
+    h = temp;
+  } else {
+    ptr = h;
+    while (ptr -> next != NULL)
+      ptr = ptr -> next;
+    ptr -> next = temp;
+  }
+  return h;
+}
+lnk * insertatpos(lnk * h, int g, int p) {
+  lnk * ptr, * temp;
+  int i;
+  temp = (lnk * ) malloc(sizeof(lnk));
+  temp -> val = g;
+  temp -> next = NULL;
+  if (h == NULL) {
+    h = temp;
+    printf("\n This is going to be first/head node");
+  } else {
+    if (p == 1) {
+      temp -> next = h;
+      h = temp;
+    } else {
+      ptr = h;
+      for (i = 1; i < p - 1 && ptr != NULL; i++)
+        ptr = ptr -> next;
+      if (ptr == NULL) {
+        printf("\n There is no such location ");
+        return h;
+      } else {
+        temp -> next = ptr -> next;
+        ptr -> next = temp;
+      }
+    }
+  }
+  return h;
 }
 
-int peek (st s) {
-	return s.ar[s.top];
+lnk * del(lnk * h, int g) {
+  lnk * pre, * ptr;
+  if (h == NULL) {
+    printf("\nNothing to delete");
+    return h;
+  } else {
+    if (h -> val == g) {
+
+      ptr = h;
+      h = h -> next;
+    } else {
+      ptr = h;
+
+      while (ptr != NULL && ptr -> val != g) {
+        pre = ptr;
+        ptr = ptr -> next;
+      }
+      if (ptr == NULL) {
+        printf("\n No Such element");
+        return h;
+      } else {
+        pre -> next = ptr -> next;
+        free(ptr);
+      }
+    }
+  }
+  return h;
 }
 
-int isFull (st s) {
-	if( s.top == SIZE-1)
-		return 1;
-	else
-		return 0;
+void disp(lnk * h) {
+  while (h != NULL) {
+    printf("%d,", h -> val);
+    h = h -> next;
+  }
+  printf("\b");
 }
 
-int isEmpty (st s) {
-	if(s.top == -1)
-		return 1;
-	else
-		return 0;
+lnk * insertbefore(lnk * h, int v) {
+  lnk * ptr, * pre, * temp;
+  int p;
+  printf("\nEnter the value of the inserting node:");
+  scanf("%d", & p);
+  temp = (lnk * ) malloc(sizeof(lnk));
+  temp -> val = p;
+  temp -> next = NULL;
+  if (h -> val == v) {
+    temp -> next = h;
+    h = temp;
+  } else {
+    ptr = h;
+    while (ptr -> val != v && ptr != NULL) {
+      pre = ptr;
+      ptr = ptr -> next;
+    }
+    temp -> next = pre -> next;
+    pre -> next = temp;
+  }
+  return h;
 }
 
-void display(st s) {
-	int index = s.top;
-	printf("-----\n");
-	while(index >= 0) {
-		printf("| %d | \n", s.ar[index--]);
-		printf("-----\n");
-	}
+//insert a node after a particular node
+
+void insertafter(lnk * h, int v) {
+  lnk * ptr, * temp;
+  int p;
+  ptr = h;
+  while (ptr -> val != v && ptr != NULL) {
+    ptr = ptr -> next;
+  }
+  printf("\nEnter the value of the inserting node:");
+  scanf("%d", & p);
+  temp = (lnk * ) malloc(sizeof(lnk));
+  temp -> next = NULL;
+  temp -> val = p;
+  printf("\n====================================");
+  if (ptr != NULL) {
+    temp -> next = ptr -> next;
+    ptr -> next = temp;
+  } else
+    printf("\nvalue not found");
 }
